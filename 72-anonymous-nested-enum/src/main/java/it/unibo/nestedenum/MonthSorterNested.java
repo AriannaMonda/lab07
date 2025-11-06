3781547892ddb4b13814e6bf10b6056879f7083d
Arianna Mondardini <arianna.mondardini@studio.unibo.it>
2025-11-06 19:15:05 +0100

    Ex. 72 Part 3: enum and fromString created

--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -1,14 +1,65 @@
 package it.unibo.nestedenum;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Locale;
 import java.util.Objects;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Implementation of {@link MonthSorter}.
  */
 public final class MonthSorterNested implements MonthSorter {
+    public static final int TRENT = 30;
+    public static final int TRENTUNO = 31;
+    public static final int VENTOTTO = 28;
+    public enum Month {
+        JANUARY(31), 
+        FEBRUARY(28), 
+        MARCH(31), 
+        APRIL(30),
+        MAY(31), 
+        JUNE(30), 
+        JULY(31), 
+        AUGUST(31),
+        SEPTEMBER(30), 
+        OCTOBER(31), 
+        NOVEMBER(30), 
+        DECEMBER(31);
+        
+        private final int days;
 
+        private Month(final int days){
+            if(days != TRENT && days != TRENTUNO && days != VENTOTTO){
+                throw new IllegalArgumentException("The number is not valid");
+            }
+            this.days = days;
+        }
+        public int getDays(){
+            return this.days;
+        }
+        
+        public Month fromString(String input){
+            if (input == null) {
+                throw new NullPointerException();
+            }
+            List<Month> listMonth = new ArrayList<>();
+            String monthLower = input.trim().toLowerCase();
+            
+
+            for (Month m : Month.values()) {
+                if(m.toString().toLowerCase().startsWith(monthLower)){
+                    listMonth.add(m);
+                }
+            }
+            if (listMonth.size()==1){
+                return listMonth.get(0);
+            }
+            throw new IllegalArgumentException("Argument not valid");
+        }
+    }
+    
     @Override
     public Comparator<String> sortByDays() {
         return null;
@@ -19,3 +70,4 @@ public final class MonthSorterNested implements MonthSorter {
         return null;
     }
 }
+
